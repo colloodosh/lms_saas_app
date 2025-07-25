b08666fe276026cc49ff6c991c3be5797e65ddd8
codhiambo <collins.odhiambo@cellulant.io>
2025-07-25 07:59:59 +0300

    Implement navigation bar

---
+++ src/app/companions/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div>
+            Companions
+
+        </div>
+    );
+};
+
+export default Page;
---
+++ src/components/navItems.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import React from 'react';
+import Link from "next/link";
+import {usePathname} from "next/navigation";
+import {cn} from "@/app/lib/utils";
+
+const navItems = [
+    {label: 'Home', href: '/'},
+    {label: 'Companions', href: '/companions'},
+    {label: 'My Journey', href: '/my-journey'},
+    {label: 'Sign In', href: '/sign-in'}
+]
+
+const NavItems = () => {
+    const pathname = usePathname();
+    return (
+        <nav className="flex item-center gap-4">
+            {navItems.map(({label, href}) => (
+                <Link href={href} key={label}
+                      className={cn(pathname === href && 'active text-primary font-semibold')}>
+                    {label}
+                </Link>
+            ))}
+        </nav>
+    );
+};
+
+export default NavItems;
