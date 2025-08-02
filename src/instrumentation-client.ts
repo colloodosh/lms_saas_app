fc6a386e89aea3628c842e6d15f9b791ac1b2f63
colloodosh <ocollinsodongo@gmail.com>
2025-08-02 09:15:23 +0300

    Add Sentry example page and API route for error testing

---
+++ src/instrumentation-client.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://26fad82c73ea1ad182a22a2b63ac8410@o4509772965871616.ingest.de.sentry.io/4509772967772240",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
